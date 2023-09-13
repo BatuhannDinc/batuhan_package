0bf4b16b5bb3cac2c5fed5e59268430f1fb69777
BatuhannDinc <batuhandinc97@gmail.com>
2023-09-13 10:38:16 +0300

    first commit

---
+++ lib/batuhan_package.dart
@@ -0,0 +1,28 @@
+library batuhan_package;
+
+import 'package:flutter/material.dart';
+
+class NameBox extends StatelessWidget {
+  final String name;
+  final double size;
+
+  NameBox({required this.name, this.size = 50.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        border: Border.all(color: Colors.blueAccent),
+      ),
+      child: Center(
+        child: Text(
+          name,
+          style: TextStyle(fontSize: size * 0.2),
+        ),
+      ),
+    );
+  }
+}
---
+++ test/batuhan_package_test.dart
@@ -0,0 +1,29 @@
+import 'package:batuhan_package/batuhan_package.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Renders name in the box', (WidgetTester tester) async {
+    // Test için NameBox widget'ını oluştur.
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: NameBox(name: 'John', size: 100),
+        ),
+      ),
+    );
+
+    // "John" adının ekranda görüntülenip görüntülenmediğini kontrol et.
+    expect(find.text('John'), findsOneWidget);
+
+    // Kare şeklinin olup olmadığını kontrol et.
+    expect(
+      tester.widget<Container>(find.byType(Container)).decoration,
+      isA<BoxDecoration>().having(
+        (box) => box.shape,
+        'shape',
+        BoxShape.rectangle,
+      ),
+    );
+  });
+}
